997f5198046486ab844dd6383c1a9cbb15dfdba9
Nevin Alcober <85alcober@gmail.com>
2026-03-30 11:36:54 +0000

    The scrollbar should not be visible it should invisble (_for element div

--- src/app/mobile/orders/page.tsx
+++ src/app/mobile/orders/page.tsx
@@ -118,7 +118,7 @@ export default function MobileOrdersPage() {
                 </Button>
             ))}
           </div>
-          <ScrollBar orientation="horizontal" />
+          <ScrollBar orientation="horizontal" className="hidden" />
         </ScrollArea>
       </header>
 
